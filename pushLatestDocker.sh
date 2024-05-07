bef3bc3838a8b89211f505b66b0435df35dda14c
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2024-05-07 16:26:23 -0400

    add container image for SPA multi-stage builds; prep for testing

--- pushLatestDocker.sh
+++ pushLatestDocker.sh
@@ -1,3 +1,11 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+: "${ZAC_IMAGE_REPO:="openziti/zac"}"
 ZAC_VERSION=$(jq -r .version package.json)
 
 if [ -z "${ZAC_VERSION}" ]; then
@@ -5,7 +13,7 @@ if [ -z "${ZAC_VERSION}" ]; then
   exit 1
 fi
 
-echo "Building ZAC version ${ZAC_VERSION} for amd64/arm64"
+echo "Building node ZAC version ${ZAC_VERSION} for amd64/arm64"
 
 docker buildx create --use --name=zac
 docker buildx build --platform linux/amd64,linux/arm64 . \
