414fe7ab3e206869bd5652bb279564b4f98ee636
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2024-08-08 11:30:53 -0400

    add usage hint to build script; require parse-version job;

--- build.sh
+++ build.sh
@@ -4,6 +4,12 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if ! (( $# ))
+then
+    echo "usage: $0 <base-href>"
+    exit 1
+fi
+
 npm install
 npm install -g @angular/cli@16.0.0-next.0
 ng build ziti-console-lib
