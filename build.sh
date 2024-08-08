a1da23892dcd9ecd9c82238898d1f32185a60681
Kenneth Bingham <kenneth.bingham@netfoundry.io>
2024-08-08 10:40:58 -0400

    checkpoint

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
 npm install
 npm install -g @angular/cli@16.0.0-next.0
 ng build ziti-console-lib
-ng build ziti-console --base-href $1 --deploy-url $1
+ng build ziti-console --base-href "$1"
